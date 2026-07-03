45a84d829aee7b7ed2f14391dc2da777ffa8cff4
Raj Soni <137527742+rajsoni23@users.noreply.github.com>
2026-07-03 20:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (33.22%) - LeetSync

---
+++ 136-single-number/single-number.c
@@ -0,0 +1,9 @@
+int singleNumber(int* nums, int numsSize) {
+    int unique = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        unique = unique ^ nums[i];
+    }
+    
+    return unique;
+}
