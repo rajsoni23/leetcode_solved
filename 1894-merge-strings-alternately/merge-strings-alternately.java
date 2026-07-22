8ebab11e06d9bc3210c25cde7996724adb0fba5d
Raj Soni <137527742+rajsoni23@users.noreply.github.com>
2026-07-22 12:42:23 +0530

    Time: 1 ms (96.25%) | Memory: 43 MB (78.59%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,14 +1,14 @@
 class Solution {
-    public String mergeAlternately(String word1, String word2) {
+    public String mergeAlternately(String word2, String word3) {
         StringBuilder merged = new StringBuilder();
-        int maxLen = Math.max(word1.length(), word2.length());
+        int maxLen = Math.max(word2.length(), word3.length());
         
         for (int i = 0; i < maxLen; i++) {
-            if (i < word1.length()) {
-                merged.append(word1.charAt(i));
-            }
             if (i < word2.length()) {
                 merged.append(word2.charAt(i));
+            }
+            if (i < word3.length()) {
+                merged.append(word3.charAt(i));
             }
         }
         
